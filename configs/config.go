5df5da93a94a4c8a9d8e675320cbb7eaae5b623c
Akezhan <21172@iitu.kz>
2020-05-08 06:00:07 +0600

    fixes

--- configs/config.go
+++ configs/config.go
@@ -21,16 +21,3 @@ const postgresPassword = "postgres"
 //PostgresConnection is the connection string to the database
 var PostgresConnection = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 	postgresAddress, postgresPort, postgresUsername, postgresPassword, postgresDataBase)
-
-//RpcServerAddress is an address to the gRPC server
-var RpcServerAddress = "server:50051"
-//var RpcServerAddress = "localhost:50051"
-
-//TokenServer is an address to the authorization
-var TokenServer = "46.101.138.224:50002"
-//rabbitUserName is the name of the user in Rabbit MQ
-const rabbitUserName = "admin"
-//rabbitPassword is the password of the user
-const rabbitPassword = "admin"
-//RabbitConnection is the connection string to the Rabbit MQ
-var RabbitConnection = fmt.Sprintf("amqp://%s:%s@46.101.138.224:5672/", rabbitUserName, rabbitPassword)
