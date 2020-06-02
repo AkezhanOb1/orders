7f299aaac81ba60bb8df782182d0a3973626728e
Akezhan <21172@iitu.kz>
2020-06-02 13:51:51 +0600

    company name and price was added

--- repositories/getBusinessServiceOrdersByEmailRepository.go
+++ repositories/getBusinessServiceOrdersByEmailRepository.go
@@ -8,7 +8,7 @@ import (
 )
 
 
-//GetBusinessServiceOrdersByEmailRepository is a repository that responsible to all the requests to DB
+//GetBusinessServiceOrdersByEmailRepository is a repository that responsible to all the requests to the database
 //about business categories
 func GetBusinessServiceOrdersByEmailRepository(ctx context.Context, request *pb.GetBusinessServiceOrdersByEmailRequest, clientID int64) (*pb.GetBusinessServiceOrdersByEmailResponse, error) {
 	conn, err := pgx.Connect(ctx, config.PostgresConnection)
@@ -17,7 +17,6 @@ func GetBusinessServiceOrdersByEmailRepository(ctx context.Context, request *pb.
 	}
 
 	defer conn.Close(ctx)
-
 
 	sqlQuery := `SELECT COUNT(1) from business_company_service_order;`
 
@@ -28,15 +27,18 @@ func GetBusinessServiceOrdersByEmailRepository(ctx context.Context, request *pb.
 		return nil, err
 	}
 
-
-
 	offset := request.Pagination.Offset
 	limit := request.Pagination.Limit
-	sqlQuery = `SELECT bc.id, bc.client_id, b.name, bc.start_at, bc.pre_paid, bc.created_at,
+	sqlQuery = `SELECT bc.id, bc.client_id, b.name, c.name, b.price, bc.start_at, bc.pre_paid, bc.created_at,
 					   bc.client_first_name, bc.client_phone_number, bc.client_phone_number_prefix,
 					   bc.client_commentary, bc.end_at, bc.business_service_id
-				FROM business_company_service_order bc inner join business_company_service b
-				on bc.business_service_id = b.id WHERE bc.client_id = $1 ORDER BY bc.id
+				FROM business_company_service_order bc 
+				INNER JOIN business_company_service b
+					on bc.business_service_id = b.id 
+	            INNER JOIN   business_company c 
+				    on b.company_id = c.id 
+			    WHERE bc.client_id = $1 
+				ORDER BY bc.id
 				OFFSET $2 LIMIT $3;`
 
 	rows, err := conn.Query(ctx, sqlQuery, clientID, offset, limit)
@@ -52,6 +54,8 @@ func GetBusinessServiceOrdersByEmailRepository(ctx context.Context, request *pb.
 			&order.BusinessServiceOrderID,
 			&order.ClientID,
 			&order.BusinessServiceName,
+			&order.BusinessCompanyName,
+			&order.Price,
 			&order.StartAt,
 			&order.PrePaid,
 			&order.CreatedAt,
